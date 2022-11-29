dc7e2ece299913a3dace9ddfc80cc74d59e9bca2
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2022-11-29 09:05:06 +0200

    Fixed most of the base parts of the page, and finished the active class for add button

--- javascript.js
+++ javascript.js
@@ -12,15 +12,36 @@ function addBookToLibrary(book) {
 }
 
 const lord = new Book('Lord of the rings','Tolkien','1000','almost');
+const lord1 = new Book('Lord of the rings','Tolkien','1000','almost');
+const lord2 = new Book('Lord of the rings','Tolkien','1000','almost');
 
 addBookToLibrary(lord);
+addBookToLibrary(lord1);
+addBookToLibrary(lord2);
 
-const booksgrid = document.getElementsByClassName("books-grid");
+const booksGrid = document.getElementById("books-grid");
+const addBtn = document.getElementById('btn');
+const overlayDiv = document.getElementById('overlay');
+const formDiv = document.getElementById('addBookForm');
 
+console.log(addBtn);
+
+addBtn.addEventListener('click', () => {
+    formDiv.classList.add('active');
+    overlayDiv.classList.add('active');
+});
+
+overlayDiv.addEventListener('click', (e) =>{
+    if(e.target === overlayDiv)
+    {
+        formDiv.classList.remove('active');
+        overlayDiv.classList.remove('active');
+    }
+});
 
 const displayBook = function () {
     myLibrary.forEach(element => {
-        let bookDiv = document.createElement("book-div");
+        let bookDiv = document.createElement("div");
         bookDiv.classList.add("book")
         let bookTitle = document.createElement("p");
         bookTitle.innerText = element.title;
@@ -31,7 +52,7 @@ const displayBook = function () {
         bookDiv.appendChild(bookTitle);
         bookDiv.appendChild(bookAuthor);
         bookDiv.appendChild(bookPages);
-        bookDiv.append();
+        booksGrid.appendChild(bookDiv);
     });
 }
 
